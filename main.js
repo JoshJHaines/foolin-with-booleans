ee4bfc5584035bd8cb15c75e34ece9c2a66efc19
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-19 12:39:00 -0500

    created function 'isInDanger'. Passed Test

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
-function moreThan5(num){
-  return num > 5
+function moreThan5(num) {
+	return num > 5;
 }
 
-function isNewTopScore(score, topScore){
-  return score > topScore
+function isNewTopScore(score, topScore) {
+	return score > topScore;
 }
 
-
-
+function isInDanger(grade) {
+	return grade >= 60 && grade <= 71;
+}
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
@@ -18,77 +19,75 @@ function isNewTopScore(score, topScore){
  *                          DON'T ADD TO OR CHANGE ANYTHING BELOW!                          *
  ********************************************************************************************/
 
-
-if (typeof moreThan5 === 'undefined') {
-  moreThan5 = undefined;
+if (typeof moreThan5 === "undefined") {
+	moreThan5 = undefined;
 }
 
-if (typeof isNewTopScore === 'undefined') {
-  isNewTopScore = undefined;
+if (typeof isNewTopScore === "undefined") {
+	isNewTopScore = undefined;
 }
 
-if (typeof isInDanger === 'undefined') {
-  isInDanger = undefined;
+if (typeof isInDanger === "undefined") {
+	isInDanger = undefined;
 }
 
-if (typeof isCoasting === 'undefined') {
-  isCoasting = undefined;
+if (typeof isCoasting === "undefined") {
+	isCoasting = undefined;
 }
 
-if (typeof isSucceeding === 'undefined') {
-  isSucceeding = undefined;
+if (typeof isSucceeding === "undefined") {
+	isSucceeding = undefined;
 }
 
-if (typeof isFailing === 'undefined') {
-  isFailing = undefined;
+if (typeof isFailing === "undefined") {
+	isFailing = undefined;
 }
 
-if (typeof isAcing === 'undefined') {
-  isAcing = undefined;
+if (typeof isAcing === "undefined") {
+	isAcing = undefined;
 }
 
-if (typeof isStudent === 'undefined') {
-  isStudent = undefined;
+if (typeof isStudent === "undefined") {
+	isStudent = undefined;
 }
 
-if (typeof isTeacher === 'undefined') {
-  isTeacher = undefined;
+if (typeof isTeacher === "undefined") {
+	isTeacher = undefined;
 }
 
-if (typeof isAdmin === 'undefined') {
-  isAdmin = undefined;
+if (typeof isAdmin === "undefined") {
+	isAdmin = undefined;
 }
 
-if (typeof isElementary === 'undefined') {
-  isElementary = undefined;
+if (typeof isElementary === "undefined") {
+	isElementary = undefined;
 }
 
-if (typeof areDifferentPeople === 'undefined') {
-  areDifferentPeople = undefined;
+if (typeof areDifferentPeople === "undefined") {
+	areDifferentPeople = undefined;
 }
 
-if (typeof notAnElementarySchoolAdministrator === 'undefined') {
-  notAnElementarySchoolAdministrator = undefined;
+if (typeof notAnElementarySchoolAdministrator === "undefined") {
+	notAnElementarySchoolAdministrator = undefined;
 }
 
-if (typeof isMiddleSchoolTeacher === 'undefined') {
-  isMiddleSchoolTeacher = undefined;
+if (typeof isMiddleSchoolTeacher === "undefined") {
+	isMiddleSchoolTeacher = undefined;
 }
-
 
 module.exports = {
-  moreThan5,
-  isNewTopScore,
-  isInDanger,
-  isCoasting,
-  isSucceeding,
-  isFailing,
-  isAcing,
-  isStudent,
-  isTeacher,
-  isAdmin,
-  isElementary,
-  areDifferentPeople,
-  notAnElementarySchoolAdministrator,
-  isMiddleSchoolTeacher,
-}
+	moreThan5,
+	isNewTopScore,
+	isInDanger,
+	isCoasting,
+	isSucceeding,
+	isFailing,
+	isAcing,
+	isStudent,
+	isTeacher,
+	isAdmin,
+	isElementary,
+	areDifferentPeople,
+	notAnElementarySchoolAdministrator,
+	isMiddleSchoolTeacher,
+};
