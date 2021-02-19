7404ee65c9ba3e76fa03671f54fa5e023180b973
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-19 12:47:23 -0500

    created function 'isFailing'. Passed Test

--- main.js
+++ main.js
@@ -21,6 +21,10 @@ function isSucceeding(grade) {
 	return grade >= 84 && grade <= 92;
 }
 
+function isFailing(grade) {
+	return grade < 60;
+}
+
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
  *                              THIS IS FOR INTERNAL USE ONLY                               *
